137d2c37197f349b4ac45830fd1e3e22d0fa0525
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-09-27 21:31:33 +0200

    Some more work on memory

--- memory/src/Main.elm
+++ memory/src/Main.elm
@@ -1,22 +1,27 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 import Html.App as App
 
 
 type alias Model =
     { cards : List Card
+    , activeGame : Bool
     }
 
 
 type alias Card =
     { id : Int
     , text : String
+    , visable : Bool
     }
 
 
 type Msg
     = TurnCard
+    | StartGame
 
 
 update : Msg -> Model -> Model
@@ -25,26 +30,71 @@ update msg model =
         TurnCard ->
             model
 
+        StartGame ->
+            { model | activeGame = not model.activeGame }
+
 
 model : Model
 model =
     { cards =
-        [ { id = 0, text = "A" }
-        , { id = 1, text = "B" }
+        [ { id = 0, text = "A", visable = False }
+        , { id = 1, text = "B", visable = False }
         ]
+    , activeGame = False
     }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ cardSection model ]
+        [ startGame model
+        , cardSection model
+        ]
+
+
+startGame : Model -> Html Msg
+startGame model =
+    div []
+        [ input [ type' "button", onClick StartGame ] [ text "New Game" ]
+        ]
 
 
 cardSection : Model -> Html Msg
 cardSection model =
-    div []
-        [ text "cardSection" ]
+    let
+        newCards =
+            List.map
+                (\c ->
+                    List.repeat 2 c
+                )
+                model.cards
+                |> List.concat
+                |> randomize
+    in
+        newCards
+            |> List.map card
+            |> div [ class "container" ]
+
+randomize : List a -> List a
+randomize lis =
+    let
+        newList = []
+            List.map
+                (\x ->
+                    rand = Random.int 0 1
+                    
+                    if (rand == 0) then
+                        x ++ newList
+                    else
+                        x :: newList
+                )
+                lis
+    in
+        newList
+
+card : Card -> Html Msg
+card card =
+    div [ class "card" ] [ text card.text ]
 
 
 main : Program Never
