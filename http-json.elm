e5fad17ad4010cad9698eef24325528d89134ea4
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-09-28 21:17:02 +0200

    Setup a proj for testing Http and decoding JSON

---
+++ http-json.elm
@@ -0,0 +1,10 @@
+import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Json
+import Task
+
+type alias Model =
+    { response : }
