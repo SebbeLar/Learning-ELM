d81b7dd88cf8122551c91747ad8149fd1627e862
Sebastian Larsson <sebberlarsson@hotmail.com>
2016-09-28 21:51:34 +0200

    Made the foundation for the Http-Json proj

--- http-json.elm
+++ http-json.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
@@ -6,5 +8,63 @@ import Http
 import Json.Decode as Json
 import Task
 
+
+main : Program Never
+main =
+    App.program
+        { init = init "Hello"
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
 type alias Model =
-    { response : }
+    { response : String
+    }
+
+
+init : String -> ( Model, Cmd Msg )
+init resp =
+    ( Model resp
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Fetch
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Fetch ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ p [] [ text (toString model.response) ]
+        ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
